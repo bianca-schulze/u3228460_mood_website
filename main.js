308690f8a955db126ffc4033bf6b1ba41c0a4945
bianca-schulze <100173043+bianca-schulze@users.noreply.github.com>
2023-10-09 22:59:44 +1100

    Finally got there
    Its working - yay!

--- main.js
+++ main.js
@@ -1,42 +1,20 @@
-var video1 = "";
-var video2 = "";
-var video3 = "";
-var video4 = "";
-var video5 = "";
-var video6 = "";
-var selectedVideo = ""; 
+var apiKey = "AIzaSyCEAcHSbtmdiFrFVck042l83I83utd8CTw"; 
+var musicGenres = ["Acoustic", "Jazz", "Classical", "Pop", "RNB", "Metal"];
+var energyLevels = ["Low Energy", "High Energy"];
+var selectedMusicGenre = "";
+var selectedEnergyLevel = "";
 
-function startup() {
-  var apiKey = "AIzaSyCEAcHSbtmdiFrFVck042l83I83utd8CTw"; 
-  var keywords = ["Accoustic", "Jazz", "Classical", "Pop", "RNB", "Metal"];
+function setMusicGenre(genreIndex) {
+  selectedMusicGenre = musicGenres[genreIndex];
+}
 
-  keywords.forEach(function (keyword, index) {
-    const query = keyword + ' music';
-    const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${query}&part=snippet&type=video`;
-
-    $.getJSON(url, function (apiData) {
-      var data = apiData.items;
-      
-
-      if (data && data.length > 0) {
-        window['video' + (index + 1)] = data[0].id.videoId;
-        console.log("video" + (index + 1) + " ID = ", window['video' + (index + 1)]);
-      }
-    });
-  });
+function setEnergyLevel(levelIndex) {
+  selectedEnergyLevel = energyLevels[levelIndex];
 }
 
 function loadVideo(videoID) {
-  if (player) {
-    player.loadVideoById(videoID);
-  }
-
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
   var player;
+
   function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
       height: '390',
@@ -57,6 +35,7 @@ function loadVideo(videoID) {
   }
 
   var done = false;
+
   function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING && !done) {
       setTimeout(stopVideo, 6000);
@@ -67,52 +46,48 @@ function loadVideo(videoID) {
   function stopVideo() {
     player.stopVideo();
   }
+
+  onYouTubeIframeAPIReady();
 }
 
-// Function to set the selected video
-function setVideo(videoID) {
-  selectedVideo = videoID;
-}
-
-// Function to play the selected video
 function playSelectedVideo() {
-  if (selectedVideo) {
-    loadVideo(selectedVideo);
-    document.getElementById('videoContainer').style.display = 'block'; // Show the video player
+  if (selectedMusicGenre && selectedEnergyLevel) {
+    const query = selectedMusicGenre + " " + selectedEnergyLevel + " music";
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${query}&part=snippet&type=video`;
+
+    $.getJSON(url, function (apiData) {
+      var data = apiData.items;
+
+      if (data && data.length > 0) {
+        var videoId = data[0].id.videoId;
+        loadVideo(videoId);
+        document.getElementById('videoContainer').style.display = 'block'; // Show the video player
+      } else {
+        console.error("No video found for the selected genre and energy level.");
+      }
+    });
   } else {
-    console.error("No video selected.");
+    console.error("Please select both a music genre and an energy level.");
   }
 }
 
+// Event listeners for music genre buttons
+for (var i = 0; i < musicGenres.length; i++) {
+  (function (index) {
+    document.getElementById(`genreButton${index}`).addEventListener('click', function () {
+      setMusicGenre(index);
+    });
+  })(i);
+}
+
+// Event listeners for energy level buttons
+for (var i = 0; i < energyLevels.length; i++) {
+  (function (index) {
+    document.getElementById(`energyButton${index}`).addEventListener('click', function () {
+      setEnergyLevel(index);
+    });
+  })(i);
+}
 
 // Event listener for the "Go" button
 document.getElementById('goButton').addEventListener('click', playSelectedVideo);
-
-
-// Add functions for all six videos
-function setVideo1() {
-  setVideo(video1);
-}
-
-function setVideo2() {
-  setVideo(video2);
-}
-
-function setVideo3() {
-  setVideo(video3);
-}
-
-function setVideo4() {
-  setVideo(video4);
-}
-
-function setVideo5() {
-  setVideo(video5);
-}
-
-function setVideo6() {
-  setVideo(video6);
-}
-
-// Call the startup function to load initial video data
-startup();
