23b68c02d51d2559ee10291c976848159a615195
bianca-schulze <100173043+bianca-schulze@users.noreply.github.com>
2023-10-12 22:33:45 +1100

    Import from glitch

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var apiKey = "AIzaSyCEAcHSbtmdiFrFVck042l83I83utd8CTw";
+var apiKey = "AIzaSyBReOxd58Gg3oNFcjLrNFmRC2rsxjdFdow"; 
 var musicGenres = ["Acoustic", "Jazz", "Classical", "Pop", "RNB", "Metal"];
 var energyLevels = ["Low Energy", "High Energy"];
 var selectedMusicGenre = "";
@@ -16,17 +16,17 @@ function loadVideo(videoID) {
   var player;
 
   function onYouTubeIframeAPIReady() {
-    player = new YT.Player("player", {
-      height: "390",
-      width: "640",
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
       videoId: videoID,
       playerVars: {
-        playsinline: 1,
+        'playsinline': 1
       },
       events: {
-        onReady: onPlayerReady,
-        onStateChange: onPlayerStateChange,
-      },
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
     });
   }
 
@@ -55,17 +55,18 @@ function playSelectedVideo() {
     const query = selectedMusicGenre + " " + selectedEnergyLevel + " music";
     const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${query}&part=snippet&type=video`;
 
-    $.getJSON(url, function (apiData) {
+    $.getJSON(url, function (apiData, status) {
+      console.log("Google API call status: ", status);
       var data = apiData.items;
 
       if (data && data.length > 0) {
         var videoId = data[0].id.videoId;
         loadVideo(videoId);
-        document.getElementById("videoContainer").style.display = "block"; // Show the video player
+        document.getElementById('videoContainer').style.display = 'block'; 
+        document.getElementById('idResetbox').style.display = 'block';
+
       } else {
-        console.error(
-          "No video found for the selected genre and energy level."
-        );
+        console.error("No video found for the selected genre and energy level.");
       }
     });
   } else {
@@ -76,26 +77,41 @@ function playSelectedVideo() {
 // Event listeners for music genre buttons
 for (var i = 0; i < musicGenres.length; i++) {
   (function (index) {
-    document
-      .getElementById(`genreButton${index}`)
-      .addEventListener("click", function () {
-        setMusicGenre(index);
-      });
+    document.getElementById(`genreButton${index}`).addEventListener('click', function () {
+      setMusicGenre(index);
+    });
   })(i);
 }
 
 // Event listeners for energy level buttons
 for (var i = 0; i < energyLevels.length; i++) {
   (function (index) {
-    document
-      .getElementById(`energyButton${index}`)
-      .addEventListener("click", function () {
-        setEnergyLevel(index);
-      });
+    document.getElementById(`energyButton${index}`).addEventListener('click', function () {
+      setEnergyLevel(index);
+    });
   })(i);
 }
 
 // Event listener for the "Go" button
-document
-  .getElementById("goButton")
-  .addEventListener("click", playSelectedVideo);
+document.getElementById('goButton').addEventListener('click', playSelectedVideo);
+
+// Buttons to stay highlighted 
+var buttons = document.querySelectorAll('.btn');
+
+// Function to handle button click
+function handleButtonClick(event) {
+  event.target.classList.toggle('btn-clicked');
+}
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', handleButtonClick);
+});
+
+// Event listener for the "Go" button
+document.getElementById('goButton').addEventListener('click', playSelectedVideo);
+
+// New code for toggling background class
+document.getElementById('goButton').addEventListener('click', function () {
+  // Toggle the CSS class on the HTML element
+  document.documentElement.classList.toggle('expanded-background');
+});
